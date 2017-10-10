c38aa73f84fe8099981a92bc0545db0cccf63333
HenrySpartaGlobal <henry_koleoso@hotmail.co.uk>
2017-10-10 11:16:55 +0100

    adding logout functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
 	resources :projects
+  	devise_for :users
 
-  devise_for :users
+  	root "static#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
