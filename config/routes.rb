db5a31f27f59e1984a9c0c81bff5b8624597e4c4
HenrySpartaGlobal <henry_koleoso@hotmail.co.uk>
2017-10-10 14:06:54 +0100

    Completed Log in and log out

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 	resources :projects
   	devise_for :users
 
-  	root "static#home"
+  	root "projects#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
