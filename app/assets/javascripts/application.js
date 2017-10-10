5609b0610f120af467fc1d496b8ce0758735c294
Klein <kleinpereirakdp@googlemail.com>
2017-10-10 14:06:06 +0100

    fixed issue with jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require turbolinks
-//= require bootstrap-sprockets
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap-sprockets
 //= require_tree .
