e6d2ed7e7b64efad8638a8028c3d543ecaf36ec6
Christian Dacres <christian_dacres@hotmail.co.uk>
2017-10-10 14:30:02 +0100

    commented out belongs to user due to validation issues

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ApplicationRecord
-	belongs_to :user
+	# belongs_to :user
 	validates :title, :author, :desc, :image_url, :github_link, presence: true
 	validates :image_url, format: { with: URI.regexp, message: " must contain a valid url" }
 	validates :github_link, format: { with: URI.regexp, message: " must contain a valid url" }
