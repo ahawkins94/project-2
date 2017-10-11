abdcb8d5e95c5187aec6efb371b3adcd155d51cc
HenrySpartaGlobal <henry_koleoso@hotmail.co.uk>
2017-10-11 13:36:05 +0100

    started AWS set up

--- Gemfile
+++ Gemfile
@@ -39,6 +39,10 @@ gem 'bootstrap-sass', '~> 3.3.6'
 
 gem 'jquery-rails'
 
+gem 'paperclip'
+
+gem 'aws-sdk', '~> 2.3'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-# u1 = User.create!(email:"testemail@pmail.com", password:"yesyes")
+u1 = User.create!(email:"testemail@pmail.com", password:"yesyes")
 
-# Project.create(title: "Project 1", author: "Barry the Chihuahua", user_id:u1.id, desc:"Sample Description", 
-# 	image_url:"https://media.mnn.com/assets/images/2014/12/gray-squirrel-uc-berkeley.jpg.560x0_q80_crop-smart.jpg", 
-# 	github_link:"https://github.com/ahawkins94")
+Project.create(title: "Project 1", author: "Barry the Chihuahua", user_id:u1.id, desc:"Sample Description", 
+	image_url:"https://media.mnn.com/assets/images/2014/12/gray-squirrel-uc-berkeley.jpg.560x0_q80_crop-smart.jpg", 
+	github_link:"https://github.com/ahawkins94")
