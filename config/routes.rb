b8374d1d404d0d68ea9ab5167561fe9105fd8b57
HenrySpartaGlobal <henry_koleoso@hotmail.co.uk>
2017-10-11 16:50:21 +0100

    completed markdown for description on projects

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   	devise_for :users
 
   	root "projects#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
