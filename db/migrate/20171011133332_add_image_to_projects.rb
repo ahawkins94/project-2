1a67492100b4313a0dd905b1cffbfe85a83d302f
Christian Dacres <christian_dacres@hotmail.co.uk>
2017-10-12 09:57:31 +0100

    removed image migration from AWS testing to resolve errors

--- db/migrate/20171011133332_add_image_to_projects.rb
+++
@@ -1,9 +0,0 @@
-class AddImageToProjects < ActiveRecord::Migration[5.1]
-  def self.up
-    add_attachment :projects, :image
-  end
-
-  def self.down
-    remove_attachment :projects, :image
-  end
-end
