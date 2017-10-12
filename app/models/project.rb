06a891ba3aabc7d453425a100886a413854bd5b1
HenrySpartaGlobal <henry_koleoso@hotmail.co.uk>
2017-10-12 16:12:38 +0100

    fixed AWS issue, can now store images on our AWS bucket

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,7 @@
 class Project < ApplicationRecord
 	# belongs_to :user
-	validates :title, :author, :desc, :image_url, :github_link, presence: true
-	validates :image_url, format: { with: URI.regexp, message: " must contain a valid url" }
+	validates :title, :author, :desc, :github_link, presence: true
+	# validates :image_url, format: { with: URI.regexp, message: " must contain a valid url" }
 	validates :github_link, format: { with: URI.regexp, message: " must contain a valid url" }
 
   	has_attached_file :image, styles: {
