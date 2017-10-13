432b7ba1af43438cf322e2d1b0775fc3a9574ae7
Christian Dacres <christian_dacres@hotmail.co.uk>
2017-10-13 11:10:30 +0100

    added image2 field, removed whitespace from development

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171011133332) do
+ActiveRecord::Schema.define(version: 20171013094323) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,10 @@ ActiveRecord::Schema.define(version: 20171011133332) do
     t.string "image_content_type"
     t.integer "image_file_size"
     t.datetime "image_updated_at"
+    t.string "image2_file_name"
+    t.string "image2_content_type"
+    t.integer "image2_file_size"
+    t.datetime "image2_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
