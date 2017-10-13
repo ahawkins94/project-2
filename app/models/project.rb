4d217dc44246e019c2cb44bd9d48506c283d9fe7
Christian Dacres <christian_dacres@hotmail.co.uk>
2017-10-13 10:46:24 +0100

    added image2

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,7 +4,12 @@ class Project < ApplicationRecord
 	# validates :image_url, format: { with: URI.regexp, message: " must contain a valid url" }
 	validates :github_link, format: { with: URI.regexp, message: " must contain a valid url" }
 
-  	has_attached_file :image, styles: {
+  has_attached_file :image, styles: {
+    thumb: '100x100>',
+    square: '200x200#',
+    medium: '300x300>'
+  }
+  has_attached_file :image2, styles: {
     thumb: '100x100>',
     square: '200x200#',
     medium: '300x300>'
@@ -16,4 +21,5 @@ class Project < ApplicationRecord
 
   # Validate the attached image is image/jpg, image/png, etc
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_content_type :image2, :content_type => /\Aimage\/.*\Z/
 end
