22bad1bd514658944461d4ecb5e872187f3eb25e
Klein <kleinpereirakdp@googlemail.com>
2017-10-13 16:25:25 +0100

    resolving migate issues

--- db/schema.rb
+++ db/schema.rb
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20171013104354) do
     t.string "image_content_type"
     t.integer "image_file_size"
     t.datetime "image_updated_at"
+    t.string "tags"
     t.string "image2_file_name"
     t.string "image2_content_type"
     t.integer "image2_file_size"
@@ -36,7 +37,6 @@ ActiveRecord::Schema.define(version: 20171013104354) do
     t.string "image3_content_type"
     t.integer "image3_file_size"
     t.datetime "image3_updated_at"
-    t.string "tags"
     t.boolean "live", default: false
   end
 
